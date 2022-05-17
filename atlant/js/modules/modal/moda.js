fe777cf83ea1902ca619d9e60e72f0e22ac17534
Murad Agamedov <82538242+Murad268@users.noreply.github.com>
2022-05-17 12:50:53 +0400

    Add files via upload

---
+++ atlant/js/modules/modal/moda.js
@@ -0,0 +1,26 @@
+const modal = () => {
+   const btn = document.querySelectorAll(".openModal");
+   const modal = document.querySelector(".orderModal");
+   const exit = document.querySelector(".orderModal__box__exit");
+   const successSendBtn = document.querySelector(".successSendBtn");
+   const succesModal = document.querySelector(".successSendModal");
+   const exitModal = document.querySelector(".successSendBtnExit");
+   btn.forEach(item => {
+      item.addEventListener("click", () => {
+         modal.classList.add("orderModal__active")
+      })
+   })
+   exit.addEventListener("click", () => {
+      modal.classList.remove("orderModal__active")
+   })
+   successSendBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      succesModal.classList.add("successSendModal__active")
+   })
+   exitModal.addEventListener("click", (e) => {
+      succesModal.classList.remove("successSendModal__active")
+      modal.classList.remove("orderModal__active")
+   })
+}
+modal()
+export default modal
---
+++ atlant/js/script.js
@@ -0,0 +1,4 @@
+
+import accordeon from "./modules/accordeon/accordeon.js";
+import carousel from "./modules/carousel/carousel.js";
+import modal from "./modules/modal/moda.js";
